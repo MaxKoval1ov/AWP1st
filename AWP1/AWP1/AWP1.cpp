9fb8d410d6a0c25d15720afefe5b810ff2ed9f69
Maksim <maxkov112211@gmail.com>
2022-03-03 23:37:39 +0300

    init:init 1st task

---
+++ AWP1/AWP1/AWP1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include<ctime>
+
+
+using namespace std;
+int main()
+{
+	int row1, row2, col1, col2;
+	double **a, **b, **c;
+	int r1c2;
+	srand(time(0));
+	cout << "Rows of 1st and columns of 2nd";
+	cin >> r1c2;
+}
+
